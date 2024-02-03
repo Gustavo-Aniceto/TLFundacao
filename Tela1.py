78ed30ea4b590d73431dc8bd6de567374b834b95
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-02-02 23:46:04 -0300

    finalizar tela1

--- Tela1.py
+++ Tela1.py
@@ -1,20 +1,16 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-
 class MainApp(App):
     def build(self):
+        label = Label(
+            text='Bom dia ,Boa tarde ou Boa noite, me chamou Gustavo e sou um Dev!',
+            size_hint=(.5, .5),
+            pos_hint={'center_x': .5, 'center_y': .5}
+        )
+        return label
 
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
 
-label = Label(text=’Fala
-galera!’,
-size_hint = (.5, .5),
-pos_hint = {‘center_x’:.5
-,‘center_y’:.5})
-
-returnlabel
-
-if__name__ ==‘__main__’:
-app = MainApp
-()
-app.run()
